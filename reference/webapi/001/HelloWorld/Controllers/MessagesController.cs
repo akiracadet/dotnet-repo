18ec680d505d6fffc31e8ab165d627f2a6434477
Mavis Vermillion <mavis.vermillion.ichi@gmail.com>
2020-08-19 17:32:52 +0800

    Added HelloWorld API for reference

---
+++ reference/webapi/001/HelloWorld/Controllers/MessagesController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    [Route("api/messages")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult <IEnumerable<Message>> GetAllMessages()
+        {
+            var messages = new List<Message>
+            {
+                new Message{Id=0, Content="Message 1"},
+                new Message{Id=1, Content="Message 2"},
+                new Message{Id=2, Content="Message 3"}
+            };
+
+            return Ok(messages);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult <Message> GetMessageById(int id)
+        {
+            var message = new Message{Id=0, Content="Message 1"};
+
+            return message;
+        }
+    }
+}
---
+++ reference/webapi/001/HelloWorld/Models/Message.cs
@@ -0,0 +1,8 @@
+namespace HelloWorld.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+    }
+}
